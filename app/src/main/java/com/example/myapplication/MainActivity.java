8e9fe4e34fb97e17559f085387e1936a737febbe
Cainan <cainan.bas@gmail.com>
2026-04-13 11:30:26 -0300

    algumas mudanças no arquivo layout_preferences/linearlayout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -50,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.linearlayout);
+        //setContentView(R.layout.linearlayout);
+        setContentView(R.layout.layout_preferences);;
         buttonSave = this.findViewById(R.id.button);
         editText = this.findViewById(R.id.editText);
         myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
