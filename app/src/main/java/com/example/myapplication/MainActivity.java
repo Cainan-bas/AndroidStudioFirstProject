e23f30dd538fc65bcfb22a15e704fe928b4592d4
Cainan <cainan.bas@gmail.com>
2026-04-22 11:16:19 -0300

    Configuração de permissões

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,31 +1,48 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.LocaleList;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Magnifier;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.os.LocaleListCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     //protected EditText editText;
-    private Button buttonSave=null;
-    private EditText editText=null;
-    private SharedPreferences myPrefs=null;
+    //private Button buttonSave=null;
+    //private EditText editText=null;
+    //private SharedPreferences myPrefs=null;
+
+    private static final int REQUEST_CODE = 100;
+    private static final int REQUEST_TO_MULTIPLE_PERMISSIONS = 1;
     /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }*/
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -87,22 +104,57 @@ public class MainActivity extends AppCompatActivity {
                 ePrefs.commit();
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_ola_mundo, menu);
-        return true;
-    }
+    }*/
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-        if(id==R.id.menu){
-            Log.i("TAG","Settings");
-            return true;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.layout_preferences);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener( v -> {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                //if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
+                } else {
+                    Toast.makeText(this, "Permissao ja concedida 1", Toast.LENGTH_SHORT).show();
+
+                }
+            } else {
+                Toast.makeText(this, "Permissao ja concedida anteriormente", Toast.LENGTH_SHORT).show();
+            }
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                if(Environment.isExternalStorageManager()){
+                    Toast.makeText(this, "Permissao ja concedida 2", Toast.LENGTH_SHORT).show();
+                } else {
+                    //Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    //Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    //intent.setData(uri);
+                    startActivity(intent);
+
+                }
+            }
+        });
+    }
+
+    private boolean checkAndRequestPermission() {
+        int diskPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        List<String> listPermissionNeeded = new ArrayList<>();
+
+        if (diskPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
-        return super.onOptionsItemSelected(item);
+
+        if (!listPermissionNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionNeeded.toArray(new String[listPermissionNeeded.size()]), REQUEST_TO_MULTIPLE_PERMISSIONS
+            );
+            return false;
+        }
+
+        return true;
     }
 }
